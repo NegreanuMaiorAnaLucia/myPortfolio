0a3e242806f51ac50ac81f2df9309b4fc902a52b
AnaNegreanuMaior <99827206+NegreanuMaiorAnaLucia@users.noreply.github.com>
2023-06-24 10:42:45 +0300

    finished day-theme

--- script.js
+++ script.js
@@ -18,38 +18,43 @@ window.onscroll = function() {
 // and its value is assigned to a variable named top.
 // **If the value of top is greater than or equal to 100, it adds a class of navbarDark to the header.
 
-//below is the code to activate the day-night switch
 const switchButton = document.getElementById('switch');
-const body = document.body;
+const section_header = document.getElementById('home');
+const section_about = document.getElementById('about');
+const section_services = document.getElementById('services');
+const section_portfolio = document.getElementById('portfolio');
+const section_contact = document.getElementById('contact');
 
 switchButton.addEventListener('change', function() {
-  if (this.checked) {
-    body.classList.add('day-mode');
-  } else {
-    body.classList.remove('day-mode');
-  }
-});
+    if (this.checked) {
+        section_header.classList.add('day-mode1');
+        section_header.classList.remove('night-mode');
 
-// In this JavaScript code, we select the switch button element using 
-// document.getElementById('switch'). We also select the body element 
-// using document.body to modify its class.
+        section_about.classList.add('day-mode2');
+        section_about.classList.remove('night-mode');
 
-// We add an event listener to the switch button using 
-// addEventListener('change', function() {...}). 
-// Inside the event listener function, we check if 
-// the switch button is checked (this.checked). If 
-// it is checked, we add the class 'night-mode' to 
-// the body element using body.classList.add('night-mode'). 
-// If it is not checked, we remove the class 'night-mode' from 
-// the body element using body.classList.remove('night-mode').
+        section_services.classList.add('day-mode3');
+        section_services.classList.remove('night-mode');
 
-// To activate the day-night switch, you need to include the 
-// JavaScript code in a <script> tag after the HTML code. 
-// Make sure the script is placed before the closing </body> tag 
-// or in the <head> section with a DOMContentLoaded event listener 
-// to ensure the JavaScript is executed after the HTML elements are loaded.
+        section_portfolio.classList.add('day-mode4');
+        section_portfolio.classList.remove('night-mode');
 
-// Note: The above JavaScript code assumes that you have defined 
-// the night mode styles in your CSS by adding a class called 'night-mode'. 
-// You can customize the styles for the night mode by adding rules under 
-// .night-mode selector in your CSS.
+        section_contact.classList.add('day-mode5');
+        section_contact.classList.remove('night-mode');
+      } else {
+        section_header.classList.add('night-mode');
+        section_header.classList.remove('day-mode1');
+
+        section_about.classList.add('night-mode');
+        section_about.classList.remove('day-mode2');
+
+        section_services.classList.add('night-mode');
+        section_services.classList.remove('day-mode3');
+
+        section_portfolio.classList.add('night-mode');
+        section_portfolio.classList.remove('day-mode4');
+
+        section_contact.classList.add('night-mode');
+        section_contact.classList.remove('day-mode5');
+      }
+  });
