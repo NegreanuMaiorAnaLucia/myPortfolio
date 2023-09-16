3a7af09ee58e5bd114891494ff55b0a545025fac
AnaNegreanuMaior <99827206+NegreanuMaiorAnaLucia@users.noreply.github.com>
2023-09-16 09:36:49 +0300

    changes made to make code more efficient

--- script.js
+++ script.js
@@ -18,43 +18,16 @@ window.onscroll = function() {
 // and its value is assigned to a variable named top.
 // **If the value of top is greater than or equal to 100, it adds a class of navbarDark to the header.
 
-const switchButton = document.getElementById('switch');
-const section_header = document.getElementById('home');
-const section_about = document.getElementById('about');
-const section_services = document.getElementById('services');
-const section_portfolio = document.getElementById('portfolio');
-const section_contact = document.getElementById('contact');
+const toggleButton = document.getElementById('switch');
+const styleElement = document.getElementById('style');
+let isNightMode = false;
 
-switchButton.addEventListener('change', function() {
-    if (this.checked) {
-        section_header.classList.add('day-mode1');
-        section_header.classList.remove('night-mode');
-
-        section_about.classList.add('day-mode2');
-        section_about.classList.remove('night-mode');
-
-        section_services.classList.add('day-mode3');
-        section_services.classList.remove('night-mode');
-
-        section_portfolio.classList.add('day-mode4');
-        section_portfolio.classList.remove('night-mode');
-
-        section_contact.classList.add('day-mode5');
-        section_contact.classList.remove('night-mode');
-      } else {
-        section_header.classList.add('night-mode');
-        section_header.classList.remove('day-mode1');
-
-        section_about.classList.add('night-mode');
-        section_about.classList.remove('day-mode2');
-
-        section_services.classList.add('night-mode');
-        section_services.classList.remove('day-mode3');
-
-        section_portfolio.classList.add('night-mode');
-        section_portfolio.classList.remove('day-mode4');
-
-        section_contact.classList.add('night-mode');
-        section_contact.classList.remove('day-mode5');
-      }
-  });
+toggleButton.addEventListener('change', function() {
+  if (isNightMode) {
+    styleElement.setAttribute('href', 'style_day.css');
+    isNightMode = false;
+  } else {
+    styleElement.setAttribute('href', 'style_night.css');
+    isNightMode = true;
+  }
+});
