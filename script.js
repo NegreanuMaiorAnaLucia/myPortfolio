910b3dd3d402a6421f0a3620c2a2631892793fb9
AnaNegreanuMaior <99827206+NegreanuMaiorAnaLucia@users.noreply.github.com>
2023-09-16 09:55:19 +0300

    fixed day-night mode and made it more efficient

--- script.js
+++ script.js
@@ -2,21 +2,13 @@ const header = document.querySelector('.navbar');
 
 window.onscroll = function() {
     var top = window.scrollY;
-    if(top >= 100) {
+    if(top >= 50) {
         header.classList.add('navbarDark');
     }
     else {
         header.classList.remove('navbarDark');
     }
 }
-
-// Now, let's break down the above code:
-// ** The header holds the value of the nav element since the querySelector method returns 
-// the first element that matches the CSS selector (which is .navbar in this case).
-// **window.onscroll fires up when the scroll event happens.
-// **window.scrollY returns the number of pixels that the document is scrolled vertically
-// and its value is assigned to a variable named top.
-// **If the value of top is greater than or equal to 100, it adds a class of navbarDark to the header.
 
 const toggleButton = document.getElementById('switch');
 const styleElement = document.getElementById('style');
